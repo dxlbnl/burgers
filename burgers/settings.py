af10259d8ae118706662671a278f97134858f0fa
= <=>
2014-07-09 14:08:55 +0200

    Fixed configuration issue

--- burgers/settings.py
+++ burgers/settings.py
@@ -66,6 +66,15 @@ WSGI_APPLICATION = 'burgers.wsgi.application'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
 DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
